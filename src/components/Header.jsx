3470fe1bbb5aee8f645ee8e65fc0f7113ccb7520
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2024-10-05 11:55:25 -0300

    Feito Header do site

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/logo.png"
+import './header.css'
+
+function Header(){
+    return(
+        <header>
+
+            <img class="img-logo" src={logo} alt=""/>
+
+            <nav class="menu">
+                <a href="">Promoções</a>
+                <a href="">Blogs</a>
+                <a href="">Programação</a>
+                <a href="">Anuncie</a>
+            </nav>
+
+            <aside class="time">
+                <p>12:00</p>
+            </aside>
+
+        </header>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Header from './components/Header'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
   </StrictMode>,
   
 )
